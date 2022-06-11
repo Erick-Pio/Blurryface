263d4280747ee40b36dd6bf3ab073f594fd8da73
Erick-Pio <erick.pio@sptech.school>
2022-06-11 19:20:51 -0300

    API conectada

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "acquatec",
-    user: "aluno",
-    password: "sptech",
+    user: "root",
+    password: "15935786a",
 };
 
 function executar(instrucao) {
